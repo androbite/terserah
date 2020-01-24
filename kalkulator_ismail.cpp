f97eb70ebea96cbd704db37c64e0ef4364b0c341
Muhamad Ismail <59985538+Mismail29@users.noreply.github.com>
2020-01-24 14:44:07 +0700

    update kalkulator

---
+++ kalkulator_ismail.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int x,y,hasil;
+	menu :
+    cout << "Kalkulator Ismail \n";
+    
+    cout << "Pilihan Menu : \n";
+    cout << "1.Penjumlahan (+)\n";
+    cout << "2.Pengurangan (-)\n";
+    cout << "3.Perkalian (*)\n";
+    cout << "4.Pembagian (/)\n\n";
+  	cout<<"ketikkan angka 1: ";
+ cin>>x;
+ 	cout<<"ketikkan angka 2: ";
+ cin>>y;
+ 
+ int z;
+ cout<<"pilih operasi perhitungan";
+ cin>>z;
+ 
+ if (z==1)
+ {
+ 	hasil=x+y;
+ 	cout<<"hasil penjumlahan = "<<hasil<<endl;
+ }
+  if (z==2)
+ {
+ 	hasil=x-y;
+ 	cout<<"hasil pengurangan = "<<hasil<<endl;
+ }
+  if (z==3)
+ {
+ 	hasil=x*y;
+ 	cout<<"hasil perkalian = "<<hasil<<endl;
+ }
+   if (z==4)
+ {
+ 	hasil=x/y;
+ 	cout<<"hasil pembagian = "<<hasil<<endl;
+ } 
+   ; 
+    cout<<"selamat mecoba" ;
+    cout<<"sekian trimakasih " ;
+    
+}
